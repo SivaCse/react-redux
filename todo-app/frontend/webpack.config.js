0a9bca6ab04fa09b7be75d65d9c79568cb6aec86
Gabriel <Gabriel>
2017-11-05 14:24:00 -0200

    start frontend

---
+++ todo-app/frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    },
+    devServer: {
+        port: 8080,
+        content: './public'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    },
+    plugins: [
+        new extractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            tests: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: extractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
+    }
+
+}
