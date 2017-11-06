b5a855a972130f6b4480ba372c128cdd1f2cb88e
Gabriel Goncalves Ribeiro <ggribeiro@stefanini.com>
2017-11-06 19:19:27 -0200

    start segundo-modulo: react-redux

---
+++ segundo-modulo/ex.3/component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Primeiro = (props) => (
+    <h1> Primeiro componente! </h1>
+)
+
+const Segundo = (props) => (
+    <h1> Segundo componente! </h1>
+)
+
+export { Primeiro, Segundo }
+
+
---
+++ segundo-modulo/ex/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Field from './field'
+
+
+ReactDOM.render(
+    <Field initialValue='Teste' />,
+    document.getElementById('app')
+);
